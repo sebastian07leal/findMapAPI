7c0fccc328d619850ef9d2dff0aad37eb4865708
sebastian07leal <sebastianleal2016@gmail.com>
2019-11-15 23:50:40 -0500

    cruds basicos probados

--- routes/favorites.js
+++ routes/favorites.js
@@ -18,6 +18,59 @@ function favoritesApi(app) {
         next(error);
     }
   });
+
+  router.get('/:favoriteID', async function(req, res, next){
+    try {
+      const favoriteID = await Promise.resolve(favoritesMocks);
+
+      res.status(200).json({
+        data: favoriteID,
+        message: 'favorite listed'
+      });
+    } catch (error){
+      next();
+    }
+  });
+
+  router.post('/', async function(req, res, next){
+    try {
+      const createFavoriteID = await Promise.resolve(favoritesMocks);
+
+      res.status(201).json({
+        data: createFavoriteID,
+        message: 'favorite created'
+      });
+    } catch (error){
+      next(error);
+    }
+  });
+
+  router.patch('/:favoriteID', async function(req, res, next){
+    try {
+      const updateFavoriteID = await Promise.resolve(favoritesMocks);
+
+      res.status(200).json({
+        data: updateFavoriteID,
+        message: 'favorite update'
+      });
+    } catch (error){
+      next(error);
+    }
+  });
+
+  router.delete('/:favoriteID', async function(req, res, next){
+    try {
+      const deleteFavoriteID = await Promise.resolve(favoritesMocks);
+
+      res.status(200).json({
+        data: deleteFavoriteID,
+        message: 'favorite deleted'
+      });
+    } catch(error){
+      next(error);
+    }
+  });
+
 }
 
 module.exports = favoritesApi;
